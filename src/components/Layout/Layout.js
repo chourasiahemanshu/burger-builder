92211db7056e78d0ccf915fddf82a9e8a779f51e
chourasiahemanshu <chourasiahemanshu@gmail.com>
2020-07-26 22:47:24 -0400

    First Commit for the burger app

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import classes from './Layout.css';
+import Aux from "../../hoc/Auxi";
+
+
+const layout = (props) => (
+    <Aux>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.Content} >
+            {props.children}
+        </main>
+    </Aux>
+);
+
+export default layout;
